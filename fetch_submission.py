f9bb6bea220f93d426993099ceb598473ac7bfaa
burioden <hashizoe.y@gmail.com>
2023-05-05 20:51:09 +0900

    special thanks追加した

--- fetch_submission.py
+++ fetch_submission.py
@@ -1,19 +1,3 @@
-'''
-
-直したいところ（こういうのにIssueを使うのか？）
-・最新AC提出をv3のAPIでもとってこれるようにしたい
-・AC以外の提出もまとめたい。いずれ「あっぷそるぶしろよ」通知botとかを作りたい…
-
-** Special thanks!! **
-
-https://zenn.dev/tishii2479/articles/6b381fb86e0369
-をもとにした
-
-いまいさん (https://twitter.com/imaimai17468) がわかんないところ答えてくれた
-
-'''
-
-
 import requests
 import json
 
