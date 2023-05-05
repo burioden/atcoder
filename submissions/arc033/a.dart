f8c30fad86d9698fe02daba93a0fecd5323a60d7
burioden <hashizoe.y@gmail.com>
2023-05-05 20:49:24 +0900

    localでエラー出るからコメントアウトした

--- submissions/arc033/a.dart
+++ submissions/arc033/a.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
+// import 'dart:io';
 
-void main() {
-  final n = int.parse(stdin.readLineSync());
-  print((n+1)*n ~/ 2);
-}
+// void main() {
+//   final n = int.parse(stdin.readLineSync());
+//   print((n + 1) * n ~/ 2);
+// }
