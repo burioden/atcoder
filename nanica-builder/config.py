407062d1b25cca429a562b7336dc014d0733d779
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-08-08 01:14:21 +0900

    main.pyの更新に合わせて更新

--- nanica-builder/config.py
+++ nanica-builder/config.py
@@ -2,122 +2,37 @@
 name:コンテスト名
 title:コンテスト名
 memo:説明文
-everyday_start_time:開始時間
-duration_second:開催時間（何分間のバチャかということ）
+start_time:開始時間
+duration_second:開催時間（何秒間のバチャかということ）
 penalty_second:ペナ
-problem_infos:
+problem_info:
  difficulty_range:diffの範囲
  include_experimental:？
- duplicate_remove_days:おそらく、今日以前この期間のものは省くという意味な気がする
+ duplicate_remove_days:(指定した期間)過去に同じ問題が出ていたら除くという設定
 '''
 
 
-# データベースのファイル名
-dbname = 'problems.sqlite3'
+DAYNIGHT_EMOJI = ["☀️", "🌙"]
+CONTEST_EMOJI = {"b": "🍰", "r": "🍘", "g": "🌶"}
+PROBLEM_MAX = {"b": "D", "r": "C", "g": "B"}
+DURATION_SECOND = {"b": 2100, "r": 3300, "g": 3300}
+START_NUM = {"b": "126", "r": "104", "g": "001"}
+LATEST_NUM = {"b": "10", "r": "2", "g": "2"}
+MAX_DIFFICULTY = {"b": 1500, "r": 4000, "g": 10000}
 
-# コンテストセット一覧
-# abc
-contest_sets_abc = [
-    {
-        'name': '☀️🍰ABCなにか(Dまで)!',
-        'title': '☀️🍰ABCなにか(Dまで)!',
-        'memo': 'だれでも参加してください！早解き練習にどうぞ / A〜D問題を35分間走るバチャ / ABC126〜最新-10までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica',
-        'everyday_start_time': '14:00',
-        'duration_second': 2100,
-        'penalty_second': 300,
-        'problem_infos': [
-            {
-                'difficulty_range': (0, 1500),
-                'include_experimental': True,
-                'duplicate_remove_days': 60,
-            }
-        ]
-    },
-    {
-        'name': '🌙🍰ABCなにか(Dまで)!',
-        'title': '🌙🍰ABCなにか(Dまで)!',
-        'memo': 'だれでも参加してください！早解き練習にどうぞ / A〜D問題を35分間走るバチャ / ABC126〜最新-10までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica',
-        'everyday_start_time': '21:00',
-        'duration_second': 2100,
-        'penalty_second': 300,
-        'problem_infos': [
-            {
-                'difficulty_range': (0, 1500),
-                'include_experimental': True,
-                'duplicate_remove_days': 60
-            }
-        ]
+def get_contest_info(contest_type, day_or_night):
+    # コンテスト情報を生成する
+    name = f"{DAYNIGHT_EMOJI[day_or_night]}{CONTEST_EMOJI[contest_type]}A{contest_type.upper()}Cなにか({PROBLEM_MAX[contest_type]}まで)!"
+    return {
+        "name": name,
+        "title": name,
+        "memo": f"だれでも参加してください！早解き練習にどうぞ / A〜{PROBLEM_MAX[contest_type]}問題を{DURATION_SECOND[contest_type]//60}分間走るバチャ / A{contest_type.upper()}C{START_NUM[contest_type]}〜最新-{LATEST_NUM[contest_type]}までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica",
+        'start_time': START_TIME[contest_type][day_or_night],
+        "duration_second": DURATION_SECOND[contest_type],
+        "penalty_second": 300,
+        "problem_info": {
+            "difficulty_range": (0, MAX_DIFFICULTY[contest_type]),
+            "include_experimental": True,
+            "duplicate_remove_days": 60
+        }
     }
-]
-
-
-
-#arc
-contest_sets_arc = [
-    {
-        'name': '☀️🍘ARCなにか(Cまで)!',
-        'title': '☀️🍘ARCなにか(Cまで)!',
-        'memo': 'だれでも参加してください！早解き練習にどうぞ / A〜C問題を55分間走るバチャ / ARC104〜最新-2までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica',
-        'everyday_start_time': '15:00',
-        'duration_second': 3300,
-        'penalty_second': 300,
-        'problem_infos': [
-            {
-                'difficulty_range': (0, 4000),
-                'include_experimental': True,
-                'duplicate_remove_days': 60,
-            }
-        ]
-    },
-    {
-        'name': '🌙🍘ARCなにか(Cまで)!',
-        'title': '🌙🍘ARCなにか(Cまで)!',
-        'memo': 'だれでも参加してください！早解き練習にどうぞ / A〜C問題を55分間走るバチャ / ARC104〜最新-2までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica',
-        'everyday_start_time': '22:00',
-        'duration_second': 3300,
-        'penalty_second': 300,
-        'problem_infos': [
-            {
-                'difficulty_range': (0, 4000),
-                'include_experimental': True,
-                'duplicate_remove_days': 60
-            }
-        ]
-    }
-]
-
-
-
-#agc
-contest_sets_agc = [
-    {
-        'name': '☀️🌶AGCなにか(Bまで)!',
-        'title': '☀️🌶AGCなにか(Bまで)!',
-        'memo': 'だれでも参加してください！早解き練習にどうぞ / A〜B問題を55分間走るバチャ / AGC001〜最新-2までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica',
-        'everyday_start_time': '15:00',
-        'duration_second': 3300,
-        'penalty_second': 300,
-        'problem_infos': [
-            {
-                'difficulty_range': (0, 10000),
-                'include_experimental': True,
-                'duplicate_remove_days': 60,
-            }
-        ]
-    },
-    {
-        'name': '🌙🌶AGCなにか(Bまで)!',
-        'title': '🌙🌶AGCなにか(Bまで)!',
-        'memo': 'だれでも参加してください！早解き練習にどうぞ / A〜B問題を55分間走るバチャ / AGC001〜最新-2までのなにかがランダムで出現 / 通知bot https://twitter.com/abc_nanica',
-        'everyday_start_time': '22:00',
-        'duration_second': 3300,
-        'penalty_second': 300,
-        'problem_infos': [
-            {
-                'difficulty_range': (0, 10000),
-                'include_experimental': True,
-                'duplicate_remove_days': 60
-            }
-        ]
-    }
-]
